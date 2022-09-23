fee2a73ea77ccd62180730e239ce26444cb9ff1b
Giles Knap <giles.knap@diamond.ac.uk>
2022-09-23 07:48:53 +0000

    pmac support building OK

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # sumo-demo
 trying out sumo - EPICS support module builder
+
+TODO - NOTE that in DEBS.DB we could change the name BASE to EPICS_BASE and remove all the aliases.
--- example.sh
+++ example.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
+cd /scratch/hgv27681/work/pmac/sumo/
 
+sumo db list
+sumo db show SSCAN
 
-sumo build new --buildtag base BASE:R7-0-6-1 SEQ:R2-2-9
-sumo build new --buildtag base2 SEQ:R2-2-9 DEVIOCSTATS:R3-1-16 BASE:R7-0-6-1 
-sumo build new --buildtag modules ASYN:R4-42 AUTOSAVE:R5-10-2 BUSY:R1-7-2 CALC:R3-7-4 SSCAN:R2-11-5 BASE:R7-0-6-1
-sumo build new --buildtag pmac PMAC:R2-6-0b2 MOTOR:R7-2-3b1 BASE:R7-0-6-1 ASYN:R4-42 AUTOSAVE:R5-10-2 BUSY:R1-7-2 CALC:R3-7-4 SSCAN:R2-11-5
+sumo build new --buildtag base    BASE:R7-0-6-1 SEQ:R2-2-9
+sumo build new --buildtag base2   BASE:R7-0-6-1 SEQ:R2-2-9 DEVIOCSTATS:R3-1-16 
+sumo build new --buildtag modules BASE:R7-0-6-1 ASYN:R4-42 AUTOSAVE:R5-10-2 BUSY:R1-7-2 CALC:R3-7-4
+sumo build new --buildtag pmac    BASE:R7-0-6-1 ASYN:R4-42 AUTOSAVE:R5-10-2 BUSY:R1-7-2 CALC:R3-7-4 SSCAN:R2-11-5 PMAC:R2-6-0b2 MOTOR:R7-2-3b1
