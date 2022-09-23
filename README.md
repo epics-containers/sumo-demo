ffc7aa082936fa15d50e7c3df59fabdafba40daa
Giles Knap <giles.knap@diamond.ac.uk>
2022-09-23 08:34:30 +0000

    added ioc

--- README.md
+++ README.md
@@ -1,4 +1,10 @@
 # sumo-demo
 trying out sumo - EPICS support module builder
 
-TODO - NOTE that in DEBS.DB we could change the name BASE to EPICS_BASE and remove all the aliases.
+Take a look at [example.sh](example.sh) to see how I have built the pmac
+support module and pmac generic IOC using sumo.
+
+Note that the system requirements for the above must be installed. To 
+get these you could use this container:
+
+podman run -it ghcr.io/epics-containers/ioc-pmac-linux-developer:dev
--- example.sh
+++ example.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
-cd /scratch/hgv27681/work/pmac/sumo/
+cd $(dirname $0)
 
 sumo db list
 sumo db show SSCAN
 
-sumo build new --buildtag base    BASE:R7-0-6-1 SEQ:R2-2-9
+# I've made some intermediate builds here that are not stricly required 
+# for the container layering we could use the base2 and pmac build tags only
+sumo build new --buildtag base    BASE:R7-0-6-1
 sumo build new --buildtag base2   BASE:R7-0-6-1 SEQ:R2-2-9 DEVIOCSTATS:R3-1-16 
 sumo build new --buildtag modules BASE:R7-0-6-1 ASYN:R4-42 AUTOSAVE:R5-10-2 BUSY:R1-7-2 CALC:R3-7-4
 sumo build new --buildtag pmac    BASE:R7-0-6-1 ASYN:R4-42 AUTOSAVE:R5-10-2 BUSY:R1-7-2 CALC:R3-7-4 SSCAN:R2-11-5 PMAC:R2-6-0b2 MOTOR:R7-2-3b1
+
+sumo build use PMAC:R2-6-0b2 -o ioc/configure/RELEASE
